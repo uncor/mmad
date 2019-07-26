c15d82650ff59b71a8a26e9ac7a0863a67ee019d
Alexis Febre <afebre@unc.edu.ar>
2019-07-26 11:11:32 -0300

    Se realizó una corrección en los valores establecidos

--- MMAD/Entities.pm
+++ MMAD/Entities.pm
@@ -20,14 +20,14 @@ sub entity{
 
         case 185    { $value = 'Rectorado' }
         case 186    { $value = 'Medicina' }
-        case 187    { $value = 'Odontología' }
-        case 188    { $value = 'Ciencias-Económicas' }
-        case 189    { $value = 'Psicología' }
+        case 187    { $value = 'Odontologia' }
+        case 188    { $value = 'Ciencias-Economicas' }
+        case 189    { $value = 'Psicologia' }
         case 190    { $value = 'Derecho-Ciencias-Sociales' }
         case 191    { $value = 'Ciencias-Agropecuarias' }
         case 192    { $value = 'Ciencias-Exactas' }
         case 193    { $value = 'FAMAF' }
-        case 194    { $value = 'Ciencias-Químicas' }
+        case 194    { $value = 'Ciencias-Quimicas' }
         case 195    { $value = 'Filosofía-Humanidades' }
         case 196    { $value = 'Observatorio' }
         case 197    { $value = 'Arquitectura' }
@@ -35,7 +35,7 @@ sub entity{
         case 200    { $value = 'Lenguas' }
         case 201    { $value = 'Artes' }
         case 206    { $value = 'Ciencias-Sociales' }
-        case 207    { $value = 'Comunicación' }
+        case 207    { $value = 'Comunicacion' }
         else        { print color('red'),"\nUnit not found. Plese choose correct option\n\n", color('reset') }
 
     } 
