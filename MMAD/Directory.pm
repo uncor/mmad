a188b7d024414b9eace0039675348c390e7cfb33
Alexis Febre <alexisfebre@gmail.com>
2019-10-31 08:28:37 -0300

    Se crean nuevos directorios para el herbario

--- MMAD/Directory.pm
+++ MMAD/Directory.pm
@@ -12,41 +12,41 @@ use Encode;
 
 our @EXPORT = (
     qw( create_directory
-        create_content )
+        create_content
+        create_handle
+        create_directory_herbarium )
 );
 
 sub create_directory {
 
     my ($row) = @_;
 
-    my $id      = $row->{'Produccion'};
-    my $type    = $row->{'Tipo'};
-    my $link    = $row->{'Link'};
-    my $code    = $row->{'Codigo'};
-    my $date    = $row->{'Fecha_Publicacion'};
-    
+    my $id      = $row->{'id'};
+    my $type    = $row->{'tipo_produccion'};
+    my $link    = $row->{'link_archivo_fulltext'};
+    my $code    = $row->{'codigo'};
+    my $date    = $row->{'anio'};
+
     my $config  = YAML::LoadFile('config.yaml');
-    my $home    = $config->{home};
-    my $source  = $config->{src};
+    my $home    = $config->{home}->{memoria};
+    my $source  = $config->{src}->{memoria};
     my $license = $config->{license};
+    my $handle  = $config->{handle};
     
     # Folders
 
-    my $unity = entity($code);
+    my $unity = entity($code, $date );
 
     my $unity_dir = "$home/$unity";
     my $type_dir  = "$unity_dir/$type";
-    my $date_dir  = "$type_dir/$date";
-    my $item_dir  = "$date_dir/item_$id";
+    my $item_dir  = "$type_dir/item_$id";
 
     unless ( mkdir( $unity_dir, 0755 ) ) {
     }
         unless ( mkdir( $type_dir, 0755 ) ) {
         }
-            unless ( mkdir( $date_dir, 0755 ) ) {
+            unless ( mkdir( $item_dir, 0755 ) ) {
             }
-                unless ( mkdir( $item_dir, 0755 ) ) {
-                }
 
     open( my $fh, ">:encoding(UTF-8)", "$item_dir/dublin_core.xml" )
         or die "Can't save > dublin_core.xml: $!";
@@ -70,6 +70,52 @@ sub create_directory {
 
 }
 
+sub create_directory_herbarium {
+
+    my ($row) = @_;
+
+    my $num     = $row->{'catalogNumber'};
+    my $path    = $row->{'path'};
+    
+    my $config      = YAML::LoadFile('config.yaml');
+    my $home        = $config->{home}->{herbario};
+    my $source      = $config->{src}->{herbario};
+    my $license     = $config->{license};
+    my $collection  = $config->{collection};
+
+    my $dir = "$home/$collection";
+    my $catalogue = "$dir/$num";
+
+    unless ( mkdir( $dir, 0755 ) ) {
+         unless ( mkdir( $catalogue, 0755 ) ) {
+        }
+    }
+
+    open( my $fh1, ">:encoding(UTF-8)", "$catalogue/dublin_core.xml")
+        or die "Can't save > dublin_core.xml: $!";
+
+    open( my $fh2, ">:encoding(UTF-8)", "$catalogue/metadata_dwc.xml")
+        or die "Can't save > metadata_dwc.xml: $!";    
+    
+    # License txt
+
+    copy( "$license", "$catalogue/license.txt" )
+        or die "Copy failed: $!";
+
+    # File content from URL or localhost
+
+    my $src = "$source/$path";
+
+    my $ff        = File::Fetch->new( uri => $src . '.jpg');
+    my $file      = $ff->fetch( to => "$catalogue/" );
+    my $file_name = $ff->file;
+
+    create_content( $catalogue, $file_name );
+
+    return ($fh1, $fh2);
+    
+}
+
 sub create_content {
 
     my ( $folder, $file ) = @_;
@@ -83,5 +129,7 @@ sub create_content {
     print $contents( $original, $license );
 
 }
+
+
 
 1;
