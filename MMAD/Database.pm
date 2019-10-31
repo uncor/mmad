9dc928be0335a8c7f3067386db8e0d188facd074
Alexis Febre <alexisfebre@gmail.com>
2019-10-31 08:26:19 -0300

    Se agregó una nueva conexión a base de datos local

--- MMAD/Database.pm
+++ MMAD/Database.pm
@@ -5,7 +5,8 @@ use Modern::Perl;
 use base 'Exporter';
 
 our @EXPORT = (
-    qw( connect_db )
+    qw( connect_db
+        connect_db2 )
 );
 
 use DBI;
@@ -19,10 +20,10 @@ sub connect_db {
 
     my $config = YAML::LoadFile( 'config.yaml' );
     my $driver   = $config->{driver};
-    my $database = $config->{database};
-    my $host     = $config->{host};
-    my $userid   = $config->{userid};
-    my $password = $config->{password};
+    my $database = $config->{database}->{eva};
+    my $host     = $config->{host}->{sigeva};
+    my $userid   = $config->{userid}->{sigeva};
+    my $password = $config->{password}->{sigeva};
 
     my $dsn      = "DBI:$driver:database=$database:host=$host";
 
@@ -30,6 +31,26 @@ sub connect_db {
         or die $DBI::errstr;
 
     return $dbh;
+}
+
+sub connect_db2 {
+
+    my ($args) = @_;
+
+    my $config = YAML::LoadFile( 'config.yaml' );
+    my $driver   = $config->{driver};
+    my $database = $config->{database}->{herbarium};
+    my $host     = $config->{host}->{herbarium};
+    my $userid   = $config->{userid}->{herbarium};
+    my $password = $config->{password}->{herbarium};
+
+    my $dsn      = "DBI:$driver:database=$database:host=$host";
+
+    my $dbh      = DBI->connect( $dsn, $userid, $password, { mysql_enable_utf8 => 1 } )
+        or die $DBI::errstr;
+
+    return $dbh; 
+    
 }
 
 1;
